c2a4a830697921012119e01ee920b8cb16235dfe
Milad Darani <57505176+miladDarani@users.noreply.github.com>
2020-06-20 15:14:56 -0500

    ASSIGNMENT1 Added Search function to assignment

---
+++ public/Assignment_1/index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Assignment 1</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+        
+        .loader-gif{
+            text-align: center;
+            margin: 0 auto;
+        }
+
+.active-cyan-2 input.form-control[type=text]:focus:not([readonly]) {
+  border-bottom: 1px solid #4dd0e1;
+  box-shadow: 0 1px 0 0 #4dd0e1;
+}
+.active-cyan input.form-control[type=text] {
+  border-bottom: 1px solid #4dd0e1;
+  box-shadow: 0 1px 0 0 #4dd0e1;
+}
+.active-cyan .fa, .active-cyan-2 .fa {
+  color: #4dd0e1;
+}
+
+    </style>
+    <script>
+// ------------------------------------------------------------------- \\
+    function getBookList(data)
+        {
+            var xhr  = new XMLHttpRequest();
+            xhr.open('GET', 'server.php');
+            xhr.responseType = 'json';
+            xhr.onreadystatechange = function ()
+            {
+                if(xhr.readyState == 4 && xhr.status == 200 ) {
+                    console.log(xhr.response);
+                    loadListView(xhr.response);
+                }
+            }
+
+            xhr.send(null);
+        }
+
+// ------------------------------------------------------------------- \\
+
+    function loadListView(books) {
+        var html = '<table class=" table table-striped">';
+        html+= `
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Genre</th>
+                <th>Num pages</th>
+                <th>Year </th>
+            </tr>
+        `;
+
+        for(var i in books) {
+
+            html+=
+            `
+                <tr>
+                    <td><a onclick="loadBook(event, this)" class= "book" href="#" data-id="${books[i].book_id}"> ${books[i].title}</a></td>
+                    <td>${books[i].author}</td>
+                    <td>${books[i].genre}</td>
+                    <td>${books[i].num_pages}</td>
+                    <td>${books[i].year_published}</td>
+                </tr>
+            `;
+        }
+        html+= "</table>";
+        setTimeout(function(){
+            document.getElementById('list').innerHTML = html;
+        },500)
+        
+    }
+// ------------------------------------------------------------------- \\
+    function loadBook(e, book){
+        console.log(book);
+        var book_id = book.getAttribute('data-id');
+
+        var xhr = new XMLHttpRequest();
+
+        var data = encodeURI('book_id=' + book_id);
+
+        xhr.responseType = 'json';
+
+        xhr.open('GET', 'server.php?' + data);
+
+        xhr.onreadystatechange = function (){
+            if(xhr.readyState == 4 && xhr.status == 200 ){
+                loadDetail(xhr.response);
+            }
+        }
+
+        xhr.send(null);
+    }    
+
+// ------------------------------------------------------------------- \\
+    function loadDetail(book){
+        var html = 
+        `
+            <h3>${book.title}</h3>
+            <img src="images/covers/${book.image}" alt="${book.title}">
+            <p>${book.description}</p>
+            <ul>
+                <li><strong>Author: </strong> ${book.author}</li>
+                <li><strong>Author Country: </strong> ${book.author_country}</li>
+                <li><strong>Number of pages: </strong> ${book.num_pages}</li>
+                <li><strong>Year Published: </strong> ${book.year_published}</li>
+                <li><strong>Price: </strong> ${book.price}</li>
+                <li><strong>Format: </strong> ${book.format}</li>
+                <li><strong>Genre: </strong> ${book.genre}</li>
+                <li><strong>Publisher City: </strong> ${book.publisher_city}</li>
+            </ul>
+
+        `;
+        document.getElementById('detail').innerHTML = html;
+    }
+// // ------------------------------------------------------------------- \\
+
+    window.onload = function(){
+        getBookList();
+        
+    }
+
+// ------------------------------------------------------------------- \\
+
+    </script>
+</head>
+<body>
+    <div class="container mt-5">
+
+
+
+
+
+        <div class="row">
+            <div class="col-sm-12">
+
+                
+                <!-- put search form here -->
+                <form class="mb-5 form-inline d-flex justify-content-center md-form form-sm active-cyan active-cyan-2 mt-2" action="server.php" method="get">
+
+                <label for="search">Search</label>
+
+                <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" name="s">
+                </form>
+
+
+            </div>
+            <div class="col-sm-6 list">
+                <div id="list">
+                    <h2 class="text-center">List view</h2>
+                    <img class="loader-gif" src="images/200.gif" alt="">
+                </div>
+            </div>
+
+            <div class="col-sm-6 detail">
+                <div id="detail">
+                    <h2 class="text-center">Detail View </h2>
+                    
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</body>
+</html>
---
+++ public/Assignment_1/server.php
@@ -0,0 +1,113 @@
+<?php
+
+// Server PHP
+
+
+
+$dbh = new PDO('sqlite:booksite.sqlite');
+
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+header('Content-Type: application/json');
+
+function searchResults($term){
+
+    //get one book
+    $query = "SELECT book.*,
+                author.name as author,
+                author.country as author_country,
+                format.name as format,
+                publisher.name as publisher,
+                publisher.city as publisher_city,
+                genre.name as genre
+                FROM
+                book
+                JOIN author using( author_id)
+                JOIN format using( format_id)
+                JOIN genre using( genre_id)
+                JOIN publisher using( publisher_id)
+                WHERE
+                book.title LIKE :term1";
+
+    $stmt = $dbh->prepare($query);
+
+    $params = array(
+        ':term1' => "%{$term}%"
+    );
+
+    $stmt->execute($params);
+
+    echo json_encode($stmt->fetch(PDO::FETCH_ASSOC));
+
+    die;  
+}
+
+if(!empty($_GET['book_id'])) {
+
+    //get one book
+    $query = "SELECT book.*,
+                author.name as author,
+                author.country as author_country,
+                format.name as format,
+                publisher.name as publisher,
+                publisher.city as publisher_city,
+                genre.name as genre
+                FROM
+                book
+                JOIN author using( author_id)
+                JOIN format using( format_id)
+                JOIN genre using( genre_id)
+                JOIN publisher using( publisher_id)
+                WHERE
+                book_id = :book_id";
+
+    $stmt = $dbh->prepare($query);
+
+    $params = array(
+        ':book_id' => intval($_GET['book_id'])
+    );
+
+    $stmt->execute($params);
+
+    echo json_encode($stmt->fetch(PDO::FETCH_ASSOC));
+
+    die;
+//-------------------------------- /if ------------------------
+} elseif(!empty($_GET['s'])){
+    $search_term = $_GET['s'];
+    $query = "SELECT * FROM `book` WHERE `title` LIKE '%{$search_term}%'";
+
+    $stmt = $dbh->prepare($query);
+    $stmt->execute();
+
+    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+    //search query in here
+    //search agaisnt author and , use 'like'
+} else {
+    //get all books
+    $query = "SELECT book.*,
+                author.name as author,
+                author.country as author_country,
+                format.name as format,
+                publisher.name as publisher,
+                publisher.city as publisher_city,
+                genre.name as genre
+                FROM
+                book
+                JOIN author using( author_id)
+                JOIN format using( format_id)
+                JOIN genre using( genre_id)
+                JOIN publisher using( publisher_id)
+                ORDER BY title ASC";
+
+    $stmt = $dbh->prepare($query);
+
+    $stmt->execute();
+
+    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+}
+
+// } catch (Exception $e) {
+//     $errors = ['error' => $e->getMessage()];
+//     echo json_encode($errors);
+// }
