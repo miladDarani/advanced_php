8baf543faeb44950ac6acc4593b7493d6a0f105e
Milad Darani <57505176+miladDarani@users.noreply.github.com>
2020-06-28 14:24:46 -0500

    ADVANCED PHP ASSIGNMENT 2a COMPLETE

--- public/Assignment_2/assignment_2/index.php
+++ public/Assignment_2/assignment_2/index.php
@@ -77,7 +77,6 @@
                                       </div>
                                       <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    
                                       </div>
                                     </div>
                                   </div>
@@ -88,7 +87,7 @@
                     </table>
                 </div>
             </div>
-            
+
         </div> <!-- /row -->
     </div> <!-- /container -->
 
